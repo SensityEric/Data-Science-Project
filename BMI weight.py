d0ae5fe098a4c3c8cd9fde8e0bf3790950510be3
Sen Sity <98569904+SensityEric@users.noreply.github.com>
2022-02-09 13:17:24 +0000

    Basic BMI Calculator

---
+++ BMI weight.py
@@ -0,0 +1,9 @@
+print("Welcome to the BMI Head Office")
+user_name = input("What is your Name: ")
+
+user_weight = float(input("Enter your weight in pounds: "))
+
+BMI = (user_weight / 2.205)
+
+
+print(f"Hello {user_name} your BMI in Kilograms is {BMI}")
